826df163462fe80f311648b87ae8e31c4366ea77
weejh <wee_jh@yahoo.com>
2016-01-07 15:33:58 +0800

    karma testing

---
+++ karma.conf.js
@@ -0,0 +1,110 @@
+// Karma configuration
+// Generated on Thu Jan 07 2016 10:40:12 GMT+0800 (SGT)
+
+module.exports = function (config) {
+
+  // Example set of browsers to run on Sauce Labs
+  // Check out https://saucelabs.com/platforms for all browser/platform combos
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  }
+
+  config.set({
+
+    sauceLabs: {
+      testName: 'Karma Test'
+    },
+    customLaunchers: customLaunchers,
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'mocha', 'sinon-chai'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'js/*.js',
+      'test/**/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'js/*.js': ['browserify'],
+      'test/**/*.js': ['browserify']
+    },
+
+    //Browserify configuration
+    browserify: {
+      debug: true,
+      transform: [
+        ['babelify', {
+          'presets': ['es2015', 'stage-0']
+        }
+       ]
+      ]
+    },
+
+    // The rest of your karma config is here
+    // ...
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'saucelabs'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: process.env.CI ? Object.keys(customLaunchers) : ['Chrome'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
---
+++ test/mytest.js
@@ -0,0 +1,54 @@
+// var assert = require('assert')
+//
+// describe("A suite is just a function", function() {
+//   var a
+//
+//   it("and so is a spec", function() {
+//     a = true
+//
+//     expect(a).toBe(true)
+//   })
+// })
+//
+// var assert = require('assert')
+// describe('Array', function () {
+//   describe('#indexOf()', function () {
+//     it('should return -1 when the value is not present', function () {
+//       assert.equal(-1, [1, 2, 3].indexOf(5))
+//       assert.equal(-1, [1, 2, 3].indexOf(0))
+//     })
+//   })
+// })
+//
+
+// import num from '../js/one'
+//
+// describe('Num', () => {
+//   it('should return 1', () => {
+//     assert.equal(num(), 1)
+//   })
+// })
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(5))
+      assert.equal(-1, [1, 2, 3].indexOf(0))
+    })
+  })
+})
+
+describe('karma test with Chai', function () {
+  it('should expose the Chai assert method', function () {
+    assert.ok('everything', 'everything is ok')
+  })
+
+  it('should expose the Chai expect method', function () {
+    expect('foo').to.not.equal('bar')
+  })
+
+  it('should expose the Chai should property', function () {
+    (1).should.not.equal(2)
+    should.exist(123)
+  })
+})
